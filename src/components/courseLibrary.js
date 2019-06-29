dbdac4e5f31600affa3d80df759ab3157bf0875a
kaylan-johnson <drkaylan@gmail.com>
2019-06-28 19:56:59 -0600

    introduced Reduct into app

--- src/components/courseLibrary.js
+++ src/components/courseLibrary.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from "react-redux";
 
 class CourseLibrary extends Component {
 
@@ -10,7 +11,7 @@ class CourseLibrary extends Component {
 
     renderCourse(course) {
         return (
-        <li className="course">
+        <li key={course.title} className="course">
             <div className="course_info">
                 <div className="course_title-container">
                     <div className="course_title">{course.title}</div>
@@ -25,13 +26,18 @@ class CourseLibrary extends Component {
     }
 
     render() {
+        this.index = 0
         return (
             <ul>
-                {this.renderCourse({"title": "Up and Running with Redis", "description": "In this course you'll learn how to work with the efficient Redis database to manage key value relationships."})}
-                {this.renderCourse({"title": "UX for Developers", "description": "This User Experience(UX) course examines how to develop a system for approaching application development and enhancing the experience for users."})}
+                {this.props.courses.map(this.renderCourse)}
            </ul>
         )
     }
 }
 
-export default CourseLibrary;
+function mapStateToProps(state) {
+    console.log(`state courses are : ${JSON.stringify(state.courses)}`)
+    return { courses: state.courses }
+}
+
+export default connect(mapStateToProps)(CourseLibrary);
