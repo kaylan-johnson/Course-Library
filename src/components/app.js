2eb843b1144dcea80453ba3d972762e408354953
kaylan-johnson <drkaylan@gmail.com>
2019-07-01 15:04:49 -0600

    initiated styles for schedule app

--- src/components/app.js
+++ src/components/app.js
@@ -4,9 +4,14 @@ import CourseLibrary from './courseLibrary';
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <h1>Course Library</h1>
+      <div className="main-container">
+      <div className="library">
+        <h1 className="library_header">Course Library</h1>
         <CourseLibrary/>
+        </div>
+        <div className="schedule">
+        <h1 className="schedule_header">My Schedule</h1>
+        </div>
       </div>
     );
   }
