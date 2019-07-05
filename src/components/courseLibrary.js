d030ce414a88c412cf6a7d812bfa059b3290b478
kaylan-johnson <drkaylan@gmail.com>
2019-07-05 17:56:35 -0600

    filtered courses we are not enrolled in

--- src/components/courseLibrary.js
+++ src/components/courseLibrary.js
@@ -58,7 +58,6 @@ class CourseLibrary extends Component {
 }
 
 function mapStateToProps(state) {
-    console.log(`state courses are : ${JSON.stringify(state.courses)}`)
     return { courses: state.courses }
 }
 
